52e5e8c593723ba83dab134eeb3f014eaa0ebe4e
Worth Baker <worthbak@gmail.com>
2018-02-11 08:44:44 -0700

    added orange annotation; simple pie chart on clusters

--- Treasure/ClusterAnnotationView.swift
+++ Treasure/ClusterAnnotationView.swift
@@ -11,19 +11,23 @@ import MapKit
 
 final class ClusterAnnotationView: MKAnnotationView {
     
-    private static let sharedClusterImage = #imageLiteral(resourceName: "cluster_empty")
-    
     override var annotation: MKAnnotation? {
         didSet {
             guard let cluster = annotation as? MKClusterAnnotation else { return }
             
             displayPriority = .defaultHigh
-            let clusterImage = ClusterAnnotationView.sharedClusterImage
             let rect = CGRect(x: 0, y: 0, width: 40, height: 40)
             let renderer = UIGraphicsImageRenderer(size: rect.size)
             
-            let countText = "\(cluster.memberAnnotations.count)"
-            let attributes = [ NSAttributedStringKey.foregroundColor: UIColor.white,
+            let totalCount = cluster.memberAnnotations.count
+            let orangeCount = cluster
+                .memberAnnotations
+                .flatMap { $0 as? MapItem }
+                .filter { $0.itemType == .orange }
+                .count
+
+            let countText = "\(totalCount)"
+            let attributes = [ NSAttributedStringKey.foregroundColor: UIColor.black,
                                NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 14)]
             let textSize = countText.size(withAttributes: attributes)
             let textRect = CGRect(x: (rect.width / 2) - (textSize.width / 2),
@@ -32,7 +36,21 @@ final class ClusterAnnotationView: MKAnnotationView {
                                   height: textSize.height)
             
             self.image = renderer.image { _ in
-                clusterImage.draw(in: rect)
+                UIColor(red: 126 / 255.0, green: 211 / 255.0, blue: 33 / 255.0, alpha: 1.0).setFill()
+                UIBezierPath(ovalIn: rect).fill()
+                
+                UIColor(red: 245 / 255.0, green: 166 / 255.0, blue: 35 / 255.0, alpha: 1.0).setFill()
+                let piePath = UIBezierPath()
+                piePath.addArc(withCenter: CGPoint(x: 20, y: 20), radius: 20,
+                               startAngle: 0, endAngle: (CGFloat.pi * 2.0 * CGFloat(orangeCount)) / CGFloat(totalCount),
+                               clockwise: true)
+                piePath.addLine(to: CGPoint(x: 20, y: 20))
+                piePath.close()
+                piePath.fill()
+                
+                UIColor.white.setFill()
+                UIBezierPath(ovalIn: CGRect(x: 8, y: 8, width: 24, height: 24)).fill()
+                
                 countText.draw(in: textRect, withAttributes: attributes)
             }
         }
--- Treasure/MapItem.swift
+++ Treasure/MapItem.swift
@@ -8,13 +8,29 @@
 import MapKit
 
 final class MapItem: NSObject, MKAnnotation {
+    enum ItemType: UInt32 {
+        case green = 0
+        case orange = 1
+        
+        var image: UIImage {
+            switch self {
+            case .green:
+                return #imageLiteral(resourceName: "annotation")
+            case .orange:
+                return #imageLiteral(resourceName: "annotation_orange")
+            }
+        }
+    }
+    
     let coordinate: CLLocationCoordinate2D
+    let itemType: ItemType
     
     var image: UIImage {
-        return #imageLiteral(resourceName: "annotation")
+        return itemType.image
     }
     
     init(coordinate: CLLocationCoordinate2D) {
         self.coordinate = coordinate
+        self.itemType = ItemType(rawValue: arc4random_uniform(2)) ?? .green
     }
 }
